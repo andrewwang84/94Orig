90b3d791271951c712cd6156a9d517f2772bf809
unknown <yanroo8444@gmail.com>
2018-08-19 20:20:45 +0800

    for deploy

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+  telegramToken: '578132627:AAHx7v1sNnq0IkLQycgakTHaNp28Y6SK3yc',
+  development: {
+    port: 3000,
+    // anything else
+  },
+  production: {
+    port: 8080,
+    // anything else
+  }
+}
+module.exports = config;
