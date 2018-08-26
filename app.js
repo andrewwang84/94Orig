b2075db05ce140c6e30f590dad4657d05125490f
unknown <yanroo8444@gmail.com>
2018-08-26 12:59:54 +0800

    line test

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
-app.use('/line', line);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
