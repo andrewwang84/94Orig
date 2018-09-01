156c356a21407274be3cfba3f24beb1f704b2854
unknown <yanroo8444@gmail.com>
2018-09-01 11:37:19 +0800

    line QQ

--- routes/line.js
+++ routes/line.js
@@ -1,44 +1,42 @@
-const line = require('@line/bot-sdk');
+const line = require('linebot');
 const express = require('express');
 const multer = require('multer');
 const crawler = require('../crawler.js');
 const router = express.Router();
 const upload = multer();
 
-const config = {
+const bot = {
+  channelId: process.env.CHANNEL_ID,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
-const client = new line.Client(config);
+const linebotParser = bot.parser();
 
-router.post('/test', line.middleware(config), (req, res) => {
-  console.log(req.body)
-  res.status(200).json(res);
-});
+router.post('/test', linebotParser);
 
-router.post('/callback', line.middleware(config), (req, res) => {
-  console.log(req.body.events)
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((res) => res.json(res))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).res.json(err);
-    });
-  res.status(200).json(res);
-});
+// router.post('/callback', line.middleware(config), (req, res) => {
+//   console.log(req.body.events)
+//   Promise
+//     .all(req.body.events.map(handleEvent))
+//     .then((res) => res.json(res))
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).res.json(err);
+//     });
+//   res.status(200).json(res);
+// });
 
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
+// function handleEvent(event) {
+//   if (event.type !== 'message' || event.message.type !== 'text') {
+//     // ignore non-text-message event
+//     return Promise.resolve(null);
+//   }
 
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+//   // create a echoing text message
+//   const echo = { type: 'text', text: event.message.text };
 
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
-}
+//   // use reply API
+//   return client.replyMessage(event.replyToken, echo);
+// }
 
 module.exports = router;
