212b77a5ea4465cb93120b10060dcd7fb70f6f69
unknown <yanroo8444@gmail.com>
2018-09-01 11:56:32 +0800

    line QQ

--- routes/line.js
+++ routes/line.js
@@ -1,42 +1,25 @@
-const line = require('linebot');
+const linebot = require('linebot');
 const express = require('express');
 const multer = require('multer');
 const crawler = require('../crawler.js');
 const router = express.Router();
 const upload = multer();
 
-const bot = line({
+const bot = linebot({
   channelId: process.env.CHANNEL_ID,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 const linebotParser = bot.parser();
 
 router.post('/test', linebotParser);
 
-// router.post('/callback', line.middleware(config), (req, res) => {
-//   console.log(req.body.events)
-//   Promise
-//     .all(req.body.events.map(handleEvent))
-//     .then((res) => res.json(res))
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500).res.json(err);
-//     });
-//   res.status(200).json(res);
-// });
-
-// function handleEvent(event) {
-//   if (event.type !== 'message' || event.message.type !== 'text') {
-//     // ignore non-text-message event
-//     return Promise.resolve(null);
-//   }
-
-//   // create a echoing text message
-//   const echo = { type: 'text', text: event.message.text };
-
-//   // use reply API
-//   return client.replyMessage(event.replyToken, echo);
-// }
+bot.on('message', function (event) {
+  event.reply(event.message.text).then(function (data) {
+    console.log('Success', data);
+  }).catch(function (error) {
+    console.log('Error', error);
+  });
+});
 
 module.exports = router;
