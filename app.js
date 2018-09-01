19ec00b3fed580212bf21695484992e5c131bec4
unknown <yanroo8444@gmail.com>
2018-09-01 12:09:56 +0800

    abandon line

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/apiRouter');
-var line = require('./routes/line');
 
 var app = express();
 
@@ -22,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
-app.use('/line', line);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
