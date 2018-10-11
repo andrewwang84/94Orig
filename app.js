85b4b4ed11854f17c922a19164d2725eaaecd490
andrewwang <andrewwang@talk2yam.com>
2018-10-11 17:19:04 +0800

    test aws

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/apiRouter');
@@ -18,6 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: false
+}));
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
