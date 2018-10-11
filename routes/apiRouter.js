c41369ceea8aa504fd836f89868d9d275abcfc63
andrewwang <andrewwang@talk2yam.com>
2018-10-11 17:10:14 +0800

    test aws

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -9,7 +9,7 @@ var request = require('request').defaults({ jar: true });
 
 router.use(function (req, res, next) {
   if (req.headers['x-amz-sns-message-type']) {
-      // req.headers['content-type'] = 'application/json;charset=UTF-8';
+      req.headers['content-type'] = 'application/json;charset=UTF-8';
   }
   next();
 });
