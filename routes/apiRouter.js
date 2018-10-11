4a5de0527f6a66ba8400920b8d2de923bfb70666
andrewwang <andrewwang@talk2yam.com>
2018-10-11 15:40:35 +0800

    test aws

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -25,6 +25,17 @@ router.post('/web', upload.array(), async function (req, res) {
   }
 });
 
+router.post('/aws-test', upload.array(), async function (req, res) {
+  try {
+    console.log(req.body)
+    
+    res.status(200).json({ url: `hi` });
+  } catch (error) {
+    res.status(500).json({ message: `${error}` });
+    return error;
+  }
+});
+
 function telegram(urls) {
   return crawler.getImage(urls);
 }
