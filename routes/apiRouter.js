43afa75d41fb027f341616462097102b6e9558ee
andrewwang <andrewwang@talk2yam.com>
2018-10-11 15:54:57 +0800

    test aws

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -30,8 +30,8 @@ router.post('/web', upload.array(), async function (req, res) {
 
 router.post('/aws-test', upload.array(), async function (req, res) {
   try {
-    console.log(req)
-    request(req.body.SubscribeURL, function (error, response, body) {
+    console.log(JSON.parse(req.body))
+    request(JSON.parse(req.body.SubscribeURL), function (error, response, body) {
       console.log(body)
     });
     res.status(200).json({ url: `hi` });
