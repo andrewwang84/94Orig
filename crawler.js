22e734f097e2750db9a8fc202ecce18c3a4faf2b
andrewwang <andrewwang@talk2yam.com>
2018-11-13 17:06:29 +0800

    conflict

--- crawler.js
+++ crawler.js
@@ -1,11 +1,7 @@
 var request = require('request');
 var request = require('request').defaults({ jar: true });
 var cheerio = require('cheerio');
-<<<<<<< HEAD
-const puppeteer = require('puppeteer');
-=======
 const puppeteer = require('./puppeteer.js')
->>>>>>> dev
 
 let getImage = async (urls) => {
   try{
