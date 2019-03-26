f70d7b461fab653da5436a52dd8f9a174f03d01e
andrewwang <andrewwang@talk2yam.com>
2019-03-26 12:00:21 +0800

    remove useless

--- routes/index.js
+++ routes/index.js
@@ -7,39 +7,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '94Origin' });
 });
 
-router.get('/yam',async function (req, res, next) {
-  try {
-    let response = [];
-    let target = [
-      'https://qu.yam.com',
-      'https://tian.yam.com',
-      'https://dq.yam.com',
-      'https://fps.yam.com'
-    ];
-
-    for (let url of target) {
-      let result = await checkYam(url);
-      response.push(result);
-    }
-
-    res.status(200).json({
-      data: response
-    });
-  } catch (error) {
-    res.status(500).json({ message: `${error}` });
-    return error;
-  }
-});
-
-async function checkYam(url) {
-  return new Promise(function (resolve, reject) {
-    request(url, function (error, response) {
-      if (error) reject(error);
-
-      let result = `${url}: ${response.statusCode}, ${response.statusMessage}`;
-      resolve(result);
-    });
-  });
-}
-
 module.exports = router;
--- telegram.js
+++ telegram.js
@@ -43,32 +43,9 @@ bot.onText(/\/help/, (msg) => {
   bot.sendMessage(chatId, '請輸入Instagram 或 Twitter 連結\n多個連結請以"換行"隔開');
 });
 
-bot.onText(/\/ping/, async (msg) => {
-  const chatId = msg.chat.id;
-  let resp = await yamCheck('yam');
-  let data = '';
-  for (let value of resp) {
-    data = `${data}\n${value}`;
-  }
-  bot.sendMessage(chatId, data);
-});
-
 bot.onText(/王彥儒/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, '好帥 <3');
-});
-
-bot.onText(/\/twicegogo/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const input = match.input;
-  let counts = input.split(" ");
-  let resp = (counts['1'] * 5) + (counts['2'] * 10) + (counts['3'] * 25);
-  let until = 50000 - resp;
-  let heart = Math.round(until/25);
-  let keyRound = Math.round(until/(27*10+72*25));
-  let key = keyRound*5;
-  let data = `目前分數: ${resp}\n還差 ${until} 分可兌換新卡\n約等於 ${heart} 個❤\n${keyRound}關鑰匙關，${key}把鑰匙`;
-  bot.sendMessage(chatId, data);
 });
 
 async function callApi(urls, route) {
@@ -82,24 +59,6 @@ async function callApi(urls, route) {
           let data = JSON.parse(body);
           data = data.url;
           resolve(data.split(","));
-        }
-      });
-    } catch (error) {
-      reject(error);
-    }
-  });
-}
-
-async function yamCheck(route) {
-  return new Promise(function (resolve, reject) {
-    try {
-      request.get(`${apiUrl}${route}`, function (error, response, body) {
-        if (error) reject(error);
-        if (response.statusCode !== 200) {
-          reject(body);
-        } else {
-          let data = JSON.parse(body);
-          resolve(data.data);
         }
       });
     } catch (error) {
