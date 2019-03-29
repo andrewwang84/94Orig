2f166d7a9f0ae6a5e63caec63d1411e5eee93ffe
AndrewWang <andrewwang84@users.noreply.github.com>
2019-03-29 14:32:54 +0800

    Update apiRouter.js

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -4,19 +4,9 @@ const crawler = require('../crawler.js');
 const router = express.Router();
 const upload = multer();
 
-router.post('/telegram', upload.array(), async function (req, res) {
+router.post('/api', upload.array(), async function (req, res) {
   try {
     console.log(req.body.url)
-    let result = await crawler.getImage(req.body.url);
-    res.status(200).json({ url: `${result}` });
-  } catch (error) {
-    res.status(500).json({ message: `${error}` });
-    return error;
-  }
-});
-
-router.post('/web', upload.array(), async function (req, res) {
-  try{
     let result = await crawler.getImage(req.body.url);
     res.status(200).json({ url: `${result}` });
   } catch (error) {
