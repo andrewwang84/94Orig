d2140c13a35dfca91e209b1bdd123a7bd4af035c
unknown <yanroo8444@gmail.com>
2020-04-26 10:02:34 +0800

    add deep check & indent adjust

--- crawler.js
+++ crawler.js
@@ -4,117 +4,117 @@ var cheerio = require('cheerio');
 const puppeteer = require('./puppeteer.js')
 
 let getImage = async (urls) => {
-  try{
-    const data = await prepareData(urls);
-    return data;
-  } catch (error) {
-    return next(error);
-  }
+    try {
+        const data = await prepareData(urls);
+        return data;
+    } catch (error) {
+        return next(error);
+    }
 }
 
 async function prepareData(urls) {
 
-  var imageUrls = [];
-  for (var i = 0; i < urls.length; i++) {
-    if (urls[i].search(/\/p\//) !== -1) {
-      try{
-        let url = await igUrl(urls[i]);
-        imageUrls.push(url);
-      } catch (error) {
-        return error;
-      }
-    }
-    if (urls[i].search(/instagram/) !== -1 && urls[i].search(/\/p\//) === -1) {
-      try{
-        let url = urls[i];
-        if (urls[i].indexOf('?') !== -1) {
-          url = urls[i].slice(0, urls[i].indexOf('?'));
+    var imageUrls = [];
+    for (var i = 0; i < urls.length; i++) {
+        if (urls[i].search(/\/p\//) !== -1) {
+            try {
+                let url = await igUrl(urls[i]);
+                imageUrls.push(url);
+            } catch (error) {
+                return error;
+            }
         }
-        imageUrls.push(await puppeteer.getStories(url));
-      } catch (error) {
-        return error;
-      }
+        if (urls[i].search(/instagram/) !== -1 && urls[i].search(/\/p\//) === -1) {
+            try {
+                let url = urls[i];
+                if (urls[i].indexOf('?') !== -1) {
+                    url = urls[i].slice(0, urls[i].indexOf('?'));
+                }
+                imageUrls.push(await puppeteer.getStories(url));
+            } catch (error) {
+                return error;
+            }
+        }
+        if (urls[i].search(/https:\/\/twitter.com/) !== -1 || urls[i].search(/https:\/\/mobile.twitter.com/) !== -1) {
+            try {
+                let url = await twitterUrl(urls[i]);
+                imageUrls.push(url);
+            } catch (error) {
+                return error;
+            }
+        }
     }
-    if (urls[i].search(/https:\/\/twitter.com/) !== -1 || urls[i].search(/https:\/\/mobile.twitter.com/) !== -1) {
-      try {
-        let url = await twitterUrl(urls[i]);
-        imageUrls.push(url);
-      } catch (error) {
-        return error;
-      }
-    }
-  }
 
-  return new Promise(function (resolve, reject) {
-    resolve(imageUrls);
-  });
+    return new Promise(function (resolve, reject) {
+        resolve(imageUrls);
+    });
 }
 
 function igUrl(url) {
-  var result = [];
-  var target = '';
-  return new Promise(function (resolve, reject) {
-    request(url, function (error, response, body) {
-      if (error) reject(error);
+    var result = [];
+    var target = '';
+    return new Promise(function (resolve, reject) {
+        request(url, function (error, response, body) {
+            if (error) reject(error);
 
-      var $ = cheerio.load(body);
-      target = $(`body > script`)[0].children[0].data;
+            var $ = cheerio.load(body);
+            target = $(`body > script`)[0].children[0].data;
 
-      while (target.indexOf(`"display_url"`) !== -1) {
-        var chopFront = target.substring(target.indexOf(`"display_url"`) + 15, target.length);
-        var currentResult = chopFront.substring(0, chopFront.indexOf(`","`));
-        target = chopFront.substring(currentResult.length, chopFront.length);
+            while (target.indexOf(`"display_url"`) !== -1) {
+                var chopFront = target.substring(target.indexOf(`"display_url"`) + 15, target.length);
+                var currentResult = chopFront.substring(0, chopFront.indexOf(`","`));
+                target = chopFront.substring(currentResult.length, chopFront.length);
 
-        currentResult = currentResult.replace(/\\u0026/g, "&");
+                currentResult = currentResult.replace(/\\u0026/g, "&");
 
-        result.push(currentResult);
-      }
+                result.push(currentResult);
+            }
 
-      target = $(`body > script`)[0].children[0].data;
-      while (target.indexOf(`"video_url"`) !== -1) {
-        chopFront = target.substring(target.indexOf(`"video_url"`) + 13, target.length);
-        currentResult = chopFront.substring(0, chopFront.indexOf(`","`));
-        target = chopFront.substring(currentResult.length, chopFront.length);
-        currentResult = currentResult.replace(/\\u0026/gi, '&');
+            target = $(`body > script`)[0].children[0].data;
+            while (target.indexOf(`"video_url"`) !== -1) {
+                chopFront = target.substring(target.indexOf(`"video_url"`) + 13, target.length);
+                currentResult = chopFront.substring(0, chopFront.indexOf(`","`));
+                target = chopFront.substring(currentResult.length, chopFront.length);
+                currentResult = currentResult.replace(/\\u0026/gi, '&');
 
-        result.push(currentResult);
-      }
+                result.push(currentResult);
+            }
 
-      if (result.length > 1) {
-        result.shift();
-      }
+            if (result.length > 1) {
+                result.shift();
+            }
 
-      resolve(result);
+            resolve(result);
+        });
     });
-  });
 }
 
 function twitterUrl(url) {
-  var result = [];
-  let mobileUrl = url;
-  let webUrl = url;
+    var result = [];
+    let mobileUrl = url;
+    let webUrl = url;
 
-  if (url.search(/https:\/\/mobile.twitter.com/) !== -1) {
-    webUrl = url.substring(url.indexOf(`https:\/\/mobile.`) + 15, url.length);
-    webUrl = `https://${webUrl}`;
-  }
+    if (url.search(/https:\/\/mobile.twitter.com/) !== -1) {
+        webUrl = url.substring(url.indexOf(`https:\/\/mobile.`) + 15, url.length);
+        webUrl = `https://${webUrl}`;
+    }
 
-  return new Promise(function (resolve, reject) {
-    // deal with img
-    request(webUrl, function (error, response, body) {
-      if (error) reject(error);
+    return new Promise(function (resolve, reject) {
+        // deal with img
+        request(webUrl, function (error, response, body) {
+            if (error) reject(error);
 
-      const $ = cheerio.load(body);
+            const $ = cheerio.load(body);
 
-      $(`.permalink-tweet-container img`).each((index, element) => {
-        if (element['attribs']['src'].indexOf('/media/') !== -1) {
-          result.push(`${element['attribs']['src']}:orig`);
-        }
-      })
+            $(`.permalink-tweet-container img`).each((index, element) => {
+                if (element['attribs']['src'].indexOf('/media/') !== -1) {
+                    result.push(`${element['attribs']['src']}:orig`);
+                }
+            })
 
-      resolve(result);
+            resolve(result);
+        });
     });
-  });
 }
 
 let getApk = async () => {
@@ -185,7 +185,48 @@ function apkcombo(url) {
     });
 }
 
+let checkDeep = async () => {
+    try {
+        const data = await prepareDeep();
+        return data;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+function prepareDeep() {
+    const url = 'https://kpopdeepfakes.net/celebrities/twice/';
+    let result = {};
+
+    const videoBlockSelector = '.kd-video-list-item';
+    const videoSelector = '.video-link-container';
+    const titleSelector = '.video-title';
+    const nameSelector = 'div.idol-info > a:nth-child(3)';
+    const timeSelector = 'div.meta-info:nth-child(2) > span';
+
+    return new Promise(function (resolve, reject) {
+        request(url, function (error, response, body) {
+            const $ = cheerio.load(body);
+
+            let vidList = $(videoBlockSelector);
+
+            vidList.each((index, element) => {
+                result[index] = {
+                    'title': $(element).find(titleSelector).text(),
+                    'name': $(element).find(nameSelector).text().split(' ')[0],
+                    'updateTime': $(element).find(timeSelector).text(),
+                    'link': $(element).find(videoSelector).attr('href')
+                }
+            })
+
+            resolve(result);
+        });
+    });
+}
+
 module.exports = {
     getImage: getImage,
-    getApk: getApk
+    getApk: getApk,
+    checkDeep: checkDeep
 };
--- telegram.js
+++ telegram.js
@@ -6,96 +6,139 @@ const bot = new TelegramBot(token, { polling: true });
 const apiUrl = require('./config.js')[app.get('env')].url;
 
 bot.onText(/https:\/\//, async (msg, match) => {
-  const chatId = msg.chat.id;
-  let target = match.input;
-  let isStory = false;
-  if (target.search(/\/p\//) === -1 && target.search(/instagram/) !== -1) {
-    isStory = true;
-  }
-
-  target = target.substring(target.indexOf(`https:`), target.length);
-  target = target.split("\n");
-
-  try{
-    if (isStory === true) {
-      bot.sendMessage(chatId, '限時動態請稍候 5 ~ 10 秒');
-    }
-    // if (target.length === 1) {
-    //   bot.sendMessage(chatId, `分享連結：https://origin94origin.herokuapp.com?url=${target[0]}`);
-    // }
-    let resp = await callApi(target, 'api/');
-    if (resp == '') {
-      resp[0] = '沒東西啦 !!';
+    const chatId = msg.chat.id;
+    console.log(chatId);
+    let target = match.input;
+    let isStory = false;
+    if (target.search(/\/p\//) === -1 && target.search(/instagram/) !== -1) {
+        isStory = true;
     }
 
-    for (var i = 0; i < resp.length; i++) {
-      bot.sendMessage(chatId, resp[i]);
+    target = target.substring(target.indexOf(`https:`), target.length);
+    target = target.split("\n");
+
+    try {
+        if (isStory === true) {
+            bot.sendMessage(chatId, '限時動態請稍候 5 ~ 10 秒');
+        }
+        // if (target.length === 1) {
+        //   bot.sendMessage(chatId, `分享連結：https://origin94origin.herokuapp.com?url=${target[0]}`);
+        // }
+        let resp = await callApi(target, 'api/');
+        if (resp == '') {
+            resp[0] = '沒東西啦 !!';
+        }
+
+        for (var i = 0; i < resp.length; i++) {
+            bot.sendMessage(chatId, resp[i]);
+        }
+    } catch (error) {
+        bot.sendMessage(chatId, `出錯了: ${error}}`);
     }
-  } catch (error) {
-    bot.sendMessage(chatId, `出錯了: ${error}}`);
-  }
 });
 
 bot.onText(/\/help/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, '請輸入Instagram 或 Twitter 連結\n多個連結請以"換行"隔開');
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, '請輸入Instagram 或 Twitter 連結\n多個連結請以"換行"隔開');
 });
 
 bot.onText(/\/apk/, async (msg) => {
-  const chatId = msg.chat.id;
+    const chatId = msg.chat.id;
+    console.log(chatId);
 
-  try {
-    let resp = await getApk();
+    try {
+        let resp = await getApk();
 
-    if (resp == '') {
-      resp[0] = '沒東西啦 !!';
+        if (resp == '') {
+            resp[0] = '沒東西啦 !!';
+        }
+
+        let msg = '';
+        for (const key in resp) {
+            let element = resp[key];
+            msg += `${key}：\n版本：${element.version}\n更新日期：${element.date}\n載點：${element.downloadLink}\n`
+        }
+
+        bot.sendMessage(chatId, msg);
+    } catch (error) {
+        bot.sendMessage(chatId, `出錯了: ${error}}`);
     }
+});
 
-    let msg = '';
-    for (const key in resp) {
-      let element = resp[key];
-      msg += `${key}：\n版本：${element.version}\n更新日期：${element.date}\n載點：${element.downloadLink}\n`
+bot.onText(/\/deep/, async (msg) => {
+    const chatId = msg.chat.id;
+    console.log(chatId);
+
+    try {
+        let resp = await checkDeep();
+
+        let msg = '';
+        let links = '';
+        for (const key in resp) {
+            let element = resp[key];
+            msg += `${element.updateTime}  ${element.name}  ${element.title}\n`;
+            links += `${element.link}\n`;
+        }
+
+        //msg += `\n${links}`;
+
+        bot.sendMessage(chatId, msg);
+    } catch (error) {
+        bot.sendMessage(chatId, `出錯了: ${error}}`);
     }
-
-    bot.sendMessage(chatId, msg);
-  } catch (error) {
-    bot.sendMessage(chatId, `出錯了: ${error}}`);
-  }
 });
 
 async function callApi(urls, route) {
-  return new Promise(function (resolve, reject) {
-    try {
-      request.post(`${apiUrl}${route}`, { form: { url: urls } }, function (error, response, body) {
-        if (error) reject(error);
-        if (response.statusCode !== 200) {
-          reject(body);
-        } else {
-          let data = JSON.parse(body);
-          data = data.url;
-          resolve(data.split(","));
+    return new Promise(function (resolve, reject) {
+        try {
+            request.post(`${apiUrl}${route}`, { form: { url: urls } }, function (error, response, body) {
+                if (error) reject(error);
+                if (response.statusCode !== 200) {
+                    reject(body);
+                } else {
+                    let data = JSON.parse(body);
+                    data = data.url;
+                    resolve(data.split(","));
+                }
+            });
+        } catch (error) {
+            reject(error);
         }
-      });
-    } catch (error) {
-      reject(error);
-    }
-  });
+    });
 }
 
 async function getApk() {
-  return new Promise(function (resolve, reject) {
-    try {
-      request.get(`${apiUrl}api/apk`, function (error, response, body) {
-        if (error) reject(error);
-        if (response.statusCode !== 200) {
-          reject(body);
-        } else {
-          let data = JSON.parse(body);
-          resolve(data.result);
+    return new Promise(function (resolve, reject) {
+        try {
+            request.get(`${apiUrl}api/apk`, function (error, response, body) {
+                if (error) reject(error);
+                if (response.statusCode !== 200) {
+                    reject(body);
+                } else {
+                    let data = JSON.parse(body);
+                    resolve(data.result);
+                }
+            });
+        } catch (error) {
+            reject(error);
         }
-      });
-    } catch (error) {
-      reject(error);
-    }
-  });
+    });
+}
+
+async function checkDeep() {
+    return new Promise(function (resolve, reject) {
+        try {
+            request.get(`${apiUrl}api/deep`, function (error, response, body) {
+                if (error) reject(error);
+                if (response.statusCode !== 200) {
+                    reject(body);
+                } else {
+                    let data = JSON.parse(body);
+                    resolve(data.result);
+                }
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
 }
