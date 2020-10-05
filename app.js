33d0870c3043ed4daa5a0e71af0b54ad405f43b5
andrewwang <yanroo8444@gmail.com>
2020-10-05 16:45:17 +0800

    content security fix

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(helmet());
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
