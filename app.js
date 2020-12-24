11a5eb3c5b3cb5b6db12709c5ccafee97f2f3cc3
andrewwang <yanroo8444@gmail.com>
2020-12-24 16:31:02 +0800

    remove line ver

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const helmet = require("helmet");
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/apiRouter');
-var lineRouter = require('./routes/lineRouter');
 
 var app = express();
 
@@ -28,7 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
-app.use('/line', lineRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
