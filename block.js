e6a67302bb0182375fae2ff4fcbbb1efe7e60cc4
andrewwang <yanroo8444@gmail.com>
2021-03-15 12:48:00 +0800

    minor adjust & line sdk upgrade

---
+++ block.js
@@ -0,0 +1,45 @@
+const blackList = [
+    'sooyaaa__',
+    'jennierubyjane',
+    'roses_are_rosie',
+    'lalalalisa_m',
+    'blackpinkofficial',
+    'lesyeuxdenini'
+];
+const greyList = {
+    'chae': 20,
+    'rose': 75,
+    'rosepark': 500,
+    'chaeyoungpark': 500,
+    'chaeyoung': 20,
+    'jennie': 75,
+    'jen': 20,
+    'kim': 30,
+    'park': 30,
+    'jenniekim': 500,
+    'rosie': 80,
+    'lalisa': 500,
+    'lisa': 75,
+    'jisookim': 500,
+    'jisoo': 75,
+    'blink': 500,
+    'black': 80,
+    'pink': 80,
+    'ink': 20,
+    'bp': 50,
+    'area': 40,
+    'blackpink': 500,
+    'inyourarea': 100,
+    'in': 35,
+    'your': 35,
+    'area': 35
+};
+const knownIds = [
+    'jenniehappines',
+];
+
+module.exports = {
+    knownIds: knownIds,
+    greyList: greyList,
+    blackList: blackList
+};
