94f604eadf31479da7b10d40bc66542e2bf827c0
andrewwang84 <yanroo8444@gmail.com>
2021-09-05 20:30:41 +0800

    fix

--- crawler.js
+++ crawler.js
@@ -111,6 +111,7 @@ function igUrl(url, uid = '') {
             let data = $(`body > script:contains("window.__additionalDataLoaded")`)[0];
             if (data === undefined) {
                 reject ('data not found');
+                return;
             }
             target = data.children[0].data;
             let userName = target.match(/"username":"([a-zA-Z0-9\.\_]+)","blocked_by_viewer":/)[1];
