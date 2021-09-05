63307f42a653a1c79ad2f0c1e716bd7472d902e0
andrewwang84 <yanroo8444@gmail.com>
2021-09-05 19:52:47 +0800

    add web anti spam

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -4,11 +4,29 @@ const crawler = require('../crawler.js');
 const router = express.Router();
 const upload = multer();
 
+const TEXT_CD = new Map();
+
 router.post('/web/', upload.array(), async function (req, res) {
     try {
         let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+        let timestamp = Date.now();
+        if (TEXT_CD.has(ip)) {
+            let cdData = TEXT_CD.get(ip);
+            if (timestamp - cdData.time > 60 * 1000) {
+                TEXT_CD.delete(ip);
+            } else {
+                throw new Error(`CD 時間冷卻中，請 1 分鐘後再試一次`);
+            }
+        }
+
         console.log(`[LOG][WEB] ${ip}`);
         let result = await crawler.getImage(req.body.url);
+
+        TEXT_CD.set(ip, {
+            'time': timestamp
+        });
+
         res.status(200).json({ url: `${result}` });
     } catch (error) {
         res.status(500).json({ message: `${error}` });
