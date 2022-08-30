d6819ae0ac6e3d98bf5d1ca9965d347640b387f2
andrewwang <yanroo8444@gmail.com>
2022-08-30 17:09:13 +0800

    adjust config

--- config.js
+++ config.js
@@ -4,8 +4,6 @@ var config = {
         insEmail: null,
         insPass: null,
         insCookies: null,
-        insEmail_2: null,
-        insCookies_2: null,
         twitterToken: null,
         adminId: [],
         maintenceMode: true,
@@ -17,8 +15,6 @@ var config = {
         insEmail: process.env.insEmail,
         insPass: process.env.insPass,
         insCookies: process.env.insCookies,
-        insEmail_2: process.env.insEmail_2,
-        insCookies_2: process.env.insCookies_2,
         twitterToken: process.env.twitterToken,
         adminId: process.env.adminId,
         maintenceMode: true,
@@ -32,8 +28,6 @@ if (process.env.NODE_ENV != 'production') {
     config.development['insEmail'] = require('./cred.js').insEmail;
     config.development['insPass'] = require('./cred.js').insPass;
     config.development['insCookies'] = require('./cred.js').insCookies;
-    config.development['insEmail_2'] = require('./cred.js').insEmail_2;
-    config.development['insCookies_2'] = require('./cred.js').insCookies_2;
     config.development['twitterToken'] = require('./cred.js').twitterToken;
     config.development['adminId'] = require('./cred.js').adminId;
 }
