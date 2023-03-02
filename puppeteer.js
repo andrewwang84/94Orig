98f5f76dbb64042aa6f343e9a618480eccebb11b
andrewwang84 <yanroo8444@gmail.com>
2023-03-02 18:31:50 +0800

    fix selector

--- puppeteer.js
+++ puppeteer.js
@@ -44,7 +44,7 @@ async function getStories(url, forceUpdate = false, uid = '') {
     let storyId = (url.match(/https:\/\/(?:www\.)?instagram.com\/stories\/[a-zA-Z0-9\.\_]+\/([0-9]+)/) == null) ? null : url.match(/https:\/\/(?:www\.)?instagram.com\/stories\/[a-zA-Z0-9\.\_]+\/([0-9]+)/)[1];
     let storiesUrl = (storyId == null) ? null : `https://www.instagram.com/stories/${userName}/${storyId}/`;
     let homeUrl = `https://www.instagram.com/${userName}/`;
-    let imgUrls = {};
+    let imgUrls = [];
 
     userName = userName.toLowerCase();
 
