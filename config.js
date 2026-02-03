e6db56ed6ae4fb203ccb6b8406d6d2f2233a9dac
andrewwang84 <yanroo8444@gmail.com>
2026-02-03 08:59:45 +0800

    update config

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ var config = {
         adminId: [],
         myId: null,
         galleryDlListPath: null,
-        galleryDlListPath2: null,
         ytDlListPath: null,
         ytDl2ListPath: null
     },
@@ -13,7 +12,6 @@ var config = {
         adminId: process.env.adminId,
         myId: process.env.myId,
         galleryDlListPath: process.env.downloadListPath,
-        galleryDlListPath2: process.env.downloadListPath2,
         ytDlListPath: process.env.downloadListPath,
         ytD2ListPath: process.env.downloadListPath
     }
@@ -24,7 +22,6 @@ if (process.env.NODE_ENV != 'production') {
     config.development['adminId'] = require('./cred.js').adminId;
     config.development['myId'] = require('./cred.js').myId;
     config.development['galleryDlListPath'] = require('./cred.js').galleryDlListPath;
-    config.development['galleryDlListPath2'] = require('./cred.js').galleryDlListPath2;
     config.development['ytDlListPath'] = require('./cred.js').ytDlListPath;
     config.development['ytDl2ListPath'] = require('./cred.js').ytDl2ListPath;
 }
